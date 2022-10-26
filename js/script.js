90f0a66566e5ff55aa56cbf2b21c81fe4b4530bd
Sebastian Trojak <si.trojak@gmail.com>
2022-10-27 00:39:37 +0200

    created basic structure with navbar

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const nav = document.querySelector(".navbar");
+	const allnNavItems = document.querySelectorAll(".navbar");
+	const navList =document.querySelector(".navbar-collapse");
+	const logo = document.querySelector('.navbar-brand')
+	
+	
+
+	function addShadow() {
+		if (window.scrollY >= 50 && window.innerWidth>=992) {
+			nav.classList.add("shadow-bg");
+			logo.style.fontSize = '0.9rem'
+			
+			
+		} else {
+			nav.classList.remove("shadow-bg");
+			logo.style.fontSize = ''
+			
+		}
+	}
+    
+	allnNavItems.forEach(item=>item.addEventListener('click', ()=>navList.classList.remove('show')))
+	
+	window.addEventListener('scroll',addShadow)
+
+});
