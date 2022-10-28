4c21c1092180986bd523d5d039d808a0c97cb5ee
Sebastian Trojak <si.trojak@gmail.com>
2022-10-28 19:41:28 +0200

    added hamburger icon style and animation

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const nav = document.querySelector(".navbar");
 	const allnNavItems = document.querySelectorAll(".navbar");
-	const navList =document.querySelector(".navbar-collapse");
-	const logo = document.querySelector('.navbar-brand')
-	
-	
+	const navList = document.querySelector(".navbar-collapse");
+	const logo = document.querySelector(".navbar-brand");
 
 	function addShadow() {
-		if (window.scrollY >= 50 && window.innerWidth>=992) {
+		if (window.scrollY >= 50 && window.innerWidth >= 992) {
 			nav.classList.add("shadow-bg");
-			logo.style.fontSize = '0.9rem'
-			
-			
+			logo.style.fontSize = "0.9rem";
 		} else {
 			nav.classList.remove("shadow-bg");
-			logo.style.fontSize = ''
-			
+			logo.style.fontSize = "";
 		}
 	}
-    
-	allnNavItems.forEach(item=>item.addEventListener('click', ()=>navList.classList.remove('show')))
-	
-	window.addEventListener('scroll',addShadow)
 
+	allnNavItems.forEach((item) =>
+		item.addEventListener("click", () => navList.classList.remove("show"))
+	);
+
+	window.addEventListener("scroll", addShadow);
+
+	document
+		.querySelector(".second-button")
+		.addEventListener("click", function () {
+			document.querySelector(".animated-icon2").classList.toggle("open");
+		});
 });
