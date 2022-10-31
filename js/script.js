da3450db906a6f3b35f29161be647e572a24cae3
Daniel <dnl.chajecki@gmail.com>
2022-10-31 17:48:09 +0100

    add sergio progress

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const nav = document.querySelector('.navbar')
-	const allnNavItems = document.querySelectorAll('.navbar')
-	const navList = document.querySelector('.navbar-collapse')
-	const logo = document.querySelector('.navbar-brand')
+document.addEventListener("DOMContentLoaded", function () {
+	const nav = document.querySelector(".navbar");
+	const allnNavItems = document.querySelectorAll(".navbar");
+	const navList = document.querySelector(".navbar-collapse");
+	const logo = document.querySelector(".navbar-brand");
 
 	function addShadow() {
 		if (window.scrollY >= 50 && window.innerWidth >= 992) {
-			nav.classList.add('shadow-bg')
-			logo.style.fontSize = '0.9rem'
+			nav.classList.add("shadow-bg");
+			logo.style.fontSize = "0.9rem";
 		} else {
-			nav.classList.remove('shadow-bg')
-			logo.style.fontSize = ''
+			nav.classList.remove("shadow-bg");
+			logo.style.fontSize = "";
 		}
 	}
 
-	allnNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+	allnNavItems.forEach((item) =>
+		item.addEventListener("click", () => navList.classList.remove("show"))
+	);
 
-	window.addEventListener('scroll', addShadow)
+	window.addEventListener("scroll", addShadow);
 
-	document.querySelector('.second-button').addEventListener('click', function () {
-		document.querySelector('.animated-icon2').classList.toggle('open')
-	})
-})
+	document
+		.querySelector(".second-button")
+		.addEventListener("click", function () {
+			document.querySelector(".animated-icon2").classList.toggle("open");
+		});
+	});
+
