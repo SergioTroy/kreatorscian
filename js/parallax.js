48a11bede1866eaac41d7aaa6897a9ec76d54b6d
Daniel <dnl.chajecki@gmail.com>
2022-11-02 22:37:43 +0100

    start parallax

---
+++ js/parallax.js
@@ -0,0 +1,11 @@
+let text = document.getElementsById('text');
+let bgc1 = document.getElementsByClassName('bgc1')
+let planets1 = document.getElementsByClassName('planets1')
+let planets2 = document.getElementsByClassName('planets2')
+let rocks = document.getElementsByClassName('rocks')
+let comet = document.getElementsByClassName('comet')
+
+window.addEventListener('scroll', function(){
+	let value = window.scrollY
+	text.style.bottom = 50 + value * -0.5 + '%'
+})
