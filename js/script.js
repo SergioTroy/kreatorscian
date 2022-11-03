27a09ad43cfa9815913a67eaded07ffd9b046191
Sebastian Trojak <si.trojak@gmail.com>
2022-11-03 19:26:30 +0100

    fixed bug with dbl click burger icon

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const nav = document.querySelector(".navbar");
-	const allnNavItems = document.querySelectorAll(".navbar");
+	const allnNavItems = document.querySelectorAll(".nav-link");
 	const navList = document.querySelector(".navbar-collapse");
 	const logo = document.querySelector(".navbar-brand");
+	const burgerIcon = document.querySelector(".animated-icon2")
+	const burgerBtn = document.querySelector(".second-button")
 
 	function addShadow() {
 		if (window.scrollY >= 50 && window.innerWidth >= 992) {
@@ -14,15 +16,25 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	}
 
+	const navOff = () => {
+		navList.classList.remove("show");
+		burgerIcon.classList.remove("open");
+	}
+
 	allnNavItems.forEach((item) =>
-		item.addEventListener("click", () => navList.classList.remove("show"))
+		item.addEventListener("click", navOff)
 	);
 
 	window.addEventListener("scroll", addShadow);
 
-	document
-		.querySelector(".second-button")
-		.addEventListener("click", function () {
-			document.querySelector(".animated-icon2").classList.toggle("open");
+	
+		burgerBtn.addEventListener("click", () => {
+			console.log(burgerBtn)
+			if(burgerIcon.classList.contains('open')==false && burgerBtn.classList.contains('collapsed')==false){
+				burgerIcon.classList.add("open");
+			}
+			else if (burgerIcon.classList.contains('open')==true && burgerBtn.classList.contains('collapsed')==true){
+				burgerIcon.classList.remove("open");
+			}
 		});
 });
