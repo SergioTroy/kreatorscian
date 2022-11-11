9d62093dc7e8e27a78145f9a7cc90d2295632d97
DanielChcki <116658627+DanielChcki@users.noreply.github.com>
2022-11-11 18:51:08 +0100

    add faq text and icon

--- js/script.js
+++ js/script.js
@@ -1,36 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const nav = document.querySelector('.navbar')
-	const allnNavItems = document.querySelectorAll('.navbar')
-	const navList = document.querySelector('.navbar-collapse')
-	const logo = document.querySelector('.navbar-brand')
+document.addEventListener("DOMContentLoaded", function () {
+	const nav = document.querySelector(".navbar");
+	const allnNavItems = document.querySelectorAll(".nav-link");
+	const navList = document.querySelector(".navbar-collapse");
+	const logo = document.querySelector(".navbar-brand");
+	const burgerIcon = document.querySelector(".animated-icon2")
+	const burgerBtn = document.querySelector(".second-button")
 
 	function addShadow() {
 		if (window.scrollY >= 50 && window.innerWidth >= 992) {
-			nav.classList.add('shadow-bg')
-			logo.style.fontSize = '0.9rem'
+			nav.classList.add("shadow-bg");
+			logo.style.fontSize = "0.9rem";
 		} else {
-			nav.classList.remove('shadow-bg')
-			logo.style.fontSize = ''
+			nav.classList.remove("shadow-bg");
+			logo.style.fontSize = "";
 		}
 	}
 
-	allnNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+	const navOff = () => {
+		navList.classList.remove("show");
+		burgerIcon.classList.remove("open");
+	}
 
-	window.addEventListener('scroll', addShadow)
+	allnNavItems.forEach((item) =>
+		item.addEventListener("click", navOff)
+	);
 
-	document.querySelector('.second-button').addEventListener('click', function () {
-		document.querySelector('.animated-icon2').classList.toggle('open')
-	})
-})
+	window.addEventListener("scroll", addShadow);
 
-let text = document.getElementsByClassName('text')
-let bgc1 = document.getElementsByClassName('bgc1')
-let planets1 = document.getElementsByClassName('planets1')
-let planets2 = document.getElementsByClassName('planets2')
-let rocks = document.getElementsByClassName('rocks')
-let comet = document.getElementsByClassName('comet')
-
-window.addEventListener('scroll', function () {
-	let value = window.scrollY
-	text.style.top = 50 + value * -0.5 + '%'
-})
+	
+		burgerBtn.addEventListener("click", () => {
+			console.log(burgerBtn)
+			if(burgerIcon.classList.contains('open')==false && burgerBtn.classList.contains('collapsed')==false){
+				burgerIcon.classList.add("open");
+			}
+			else if (burgerIcon.classList.contains('open')==true && burgerBtn.classList.contains('collapsed')==true){
+				burgerIcon.classList.remove("open");
+			}
+		});
+});
