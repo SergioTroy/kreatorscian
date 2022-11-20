447b8cab009d849ae35b703978961b0a555d20f3
Sebastian Trojak <si.trojak@gmail.com>
2022-11-20 21:28:16 +0100

    added side buttons next and prev in galleries

--- js/gallery.js
+++ js/gallery.js
@@ -1,26 +1,70 @@
 const gallery = document.querySelectorAll(".gallery img");
 const fullImg = document.querySelector(".full-img");
 const fullImgBox = document.querySelector(".full-img-box");
-const nav = document.querySelector('nav')
-const closeImgBtn = document.querySelector('.closeFullImg')
-const description = document.querySelector('.image-description')
-
+const nav = document.querySelector("nav");
+const closeImgBtn = document.querySelector(".closeFullImg");
+const description = document.querySelector(".image-description");
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+let imgPath = "";
+let imgDescription = "";
 const showPicture = (e) => {
-	let imgPath = e.target.getAttribute("src");
-    let imgDescription = e.target.getAttribute('alt');
+	imgPath = e.target.getAttribute("src");
+	imgDescription = e.target.getAttribute("alt");
 	fullImg.setAttribute("src", imgPath);
-    if(imgDescription!=null){
-        description.textContent = imgDescription
-    }
-    nav.style.display = "none";
-    fullImgBox.style.display = "flex";
-
+	if (imgDescription != null) {
+		description.textContent = imgDescription;
+	}
+	nav.style.display = "none";
+	fullImgBox.style.display = "flex";
 };
 
 const closeImg = () => {
-    nav.style.display = "block";
-    fullImgBox.style.display = "none";
-}
+	nav.style.display = "block";
+	fullImgBox.style.display = "none";
+};
+
+const nextImg = () => {
+	for (i = 0; i < gallery.length; i++) {
+		if (
+			gallery[i].getAttribute("src") == imgPath &&
+			gallery[i].getAttribute("alt") == imgDescription
+		) {
+            if (i == gallery.length-1) {
+				fullImg.setAttribute("src", gallery[gallery.length-1].getAttribute("src"));
+				description.textContent = gallery[gallery.length-1].getAttribute("alt");
+			}
+            else{
+                fullImg.setAttribute("src", gallery[i + 1].getAttribute("src"));
+                description.textContent = gallery[i + 1].getAttribute("alt");
+            }
+			
+		}
+	}
+	imgPath = fullImg.getAttribute("src");
+	imgDescription = description.textContent;
+};
+
+const prevImg = () => {
+	for (i = 0; i < gallery.length; i++) {
+		if (
+			gallery[i].getAttribute("src") == imgPath &&
+			gallery[i].getAttribute("alt") == imgDescription
+		) {
+			if (i == 0) {
+				fullImg.setAttribute("src", gallery[0].getAttribute("src"));
+				description.textContent = gallery[0].getAttribute("alt");
+			} else {
+				fullImg.setAttribute("src", gallery[i - 1].getAttribute("src"));
+				description.textContent = gallery[i - 1].getAttribute("alt");
+			}
+		}
+	}
+	imgPath = fullImg.getAttribute("src");
+	imgDescription = description.textContent;
+};
 
 gallery.forEach((item) => item.addEventListener("click", showPicture));
-closeImgBtn.addEventListener('click',closeImg)
+closeImgBtn.addEventListener("click", closeImg);
+prevBtn.addEventListener("click", prevImg);
+nextBtn.addEventListener("click", nextImg);
