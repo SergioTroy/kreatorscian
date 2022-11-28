2724f5ce398dcad085ed59b37d20376e329c58c6
Sebastian Trojak <si.trojak@gmail.com>
2022-11-28 22:16:51 +0100

    changed galleries height desktop

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,11 @@
-const images = document.querySelectorAll('.images img')
+// const images = document.querySelectorAll('.images img')
 
-window.addEventListener('scroll', ()=>{
-   images[0].style.top = '-' + (window.scrollY / 2.5) + "px"; 
-   images[1].style.top = '-' + (window.scrollY / 3.5) + "px"; 
-   images[2].style.top = '-' + (window.scrollY / 4) + "px"; 
-})
+// window.addEventListener('scroll', ()=>{
+//    images[0].style.top = '-' + (window.scrollY / 2.5) + "px"; 
+//    images[1].style.top = '-' + (window.scrollY / 3.5) + "px"; 
+//    images[2].style.top = '-' + (window.scrollY / 2.5) + "px"; 
+//    images[3].style.top = '-' + (window.scrollY / 2.5) + "px"; 
+//    images[2].style.left = (window.scrollY / 2) + "px"; 
+//    images[3].style.left = '-' + (window.scrollY / 2) + "px"; 
+   
+// })
