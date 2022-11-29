3f0099bfd8e06d8939931e1339b5851143257219
Sebastian Trojak <si.trojak@gmail.com>
2022-11-29 14:29:40 +0100

    idea with gallery bg in header

--- js/parallax.js
+++ js/parallax.js
@@ -1,11 +1,17 @@
-// const images = document.querySelectorAll('.images img')
+const images = document.querySelectorAll('.images img')
+const title  = document.querySelector('.title')
 
-// window.addEventListener('scroll', ()=>{
-//    images[0].style.top = '-' + (window.scrollY / 2.5) + "px"; 
-//    images[1].style.top = '-' + (window.scrollY / 3.5) + "px"; 
+window.addEventListener('scroll', ()=>{
+    title.style.left = (60 - window.scrollY/10) + "%"; 
+if(window.scrollY >= 20){
+    images[1].style.zIndex = 1;
+}
+else{
+    images[1].style.zIndex = 2; 
+}
 //    images[2].style.top = '-' + (window.scrollY / 2.5) + "px"; 
 //    images[3].style.top = '-' + (window.scrollY / 2.5) + "px"; 
 //    images[2].style.left = (window.scrollY / 2) + "px"; 
 //    images[3].style.left = '-' + (window.scrollY / 2) + "px"; 
    
-// })
+})
